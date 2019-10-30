17b3efaeadfed35abeaf05eddfa00c5a762bb5d0
Konstantin Gavrilov <gavrilovks@gmail.com>
2019-10-30 10:13:20 -0700

    header file init

---
+++ fillit.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fillit.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kgavrilo <kgavrilo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/29 19:55:21 by kgavrilo          #+#    #+#             */
+/*   Updated: 2019/10/30 10:12:36 by kgavrilo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FILLIT_H
+# define FILLIT_H
+# include <stdlib.h>
+# include <string.h>
+# include <fcntl.h>
+# include <unistd.h>
+#endif
