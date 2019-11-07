5904084997634724a614ecea07cd0a81b6b3458f
“Rakhmatdzhon <rabduras@student.42.us.org>
2019-11-06 19:37:03 -0800

    added check_figure function

--- tetriminos_checker.c
+++ tetriminos_checker.c
@@ -3,44 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   tetriminos_checker.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kgavrilo <kgavrilo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rabduras <rabduras@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/06 12:41:37 by kgavrilo          #+#    #+#             */
-/*   Updated: 2019/11/06 19:21:35 by kgavrilo         ###   ########.fr       */
+/*   Updated: 2019/11/06 19:36:23 by rabduras         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 #include <stdio.h>
-
-/*
-** Function to validate Tetriminos file
-** Return True if it is correct
-*/
-
-int		check_tetriminos_file(char *filename)
-{
-	int		fd;
-	char	*line;
-	int		count_lines;
-
-	fd = open(filename, O_RDONLY);
-	if (fd > 2)
-	{
-		count_lines = 0;
-		line = NULL;
-		while (get_next_line(fd, &line) > 0)
-		{
-			count_lines++;
-			if (!(check_length(line, count_lines) && check_chars(line)))
-				return (0);
-			// if (!(check_figure())) // wait for John
-			//  	return (0);
-		}
-	}
-	close(fd);
-	return (0);
-}
 
 /*
 ** Function to check length of string
@@ -72,4 +43,71 @@ static int		check_chars(char *line)
 		line++;
 	}
 	return (1);
+}
+
+static int		check_figure(char *line, int *hashes, int *min, int *max)
+{
+	int i;
+	int temp;
+	int in_range;
+
+	i = -1;
+	temp = -1;
+	in_range = 0;
+	while (++i < 4)
+	{
+		if (line[i] == '#')
+		{
+			if (*hashes == 0 || (line[i] >= *min && line[i] <= *max))
+			{
+				temp = i + 1;
+				in_range = 1;
+				if (temp == -1)
+					*min = i;
+				*hashes++;
+			}
+			else if (temp == -1 || temp == i)
+			{
+				temp = i + 1;
+				*hashes++;
+			}
+			else
+				return (0);
+		}
+	}
+	*max = temp - 1;
+	return (in_range);
+}
+
+/*
+** Function to validate Tetriminos file
+** Return True if it is correct
+*/
+
+int		check_tetriminos_file(char *filename)
+{
+	int		fd;
+	char	*line;
+	int		count_lines;
+	int 	hashes;
+	int		range[2];
+
+	fd = open(filename, O_RDONLY);
+	if (fd > 2)
+	{
+		count_lines = 0;
+		line = NULL;
+		hashes = 0;
+		range[0] = 0;
+		range[1] = 0;
+		while (get_next_line(fd, &line) > 0)
+		{
+			if (!(check_length(line, ++count_lines) && check_chars(line)))
+				return (0);
+			if (!(check_figure(line, &hashes, &range[0], &range[1])))
+				return (0);
+		}
+	}
+	close(fd);
+	return (0);
 }
